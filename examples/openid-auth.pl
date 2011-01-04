e1c1afc573bdae83eae2b18263b95303dd3bff1a
Kang-min Liu <gugod@gugod.org>
2011-01-04 18:12:41 +0800

    let the doorman middleware to provide several path helpers

--- examples/openid-auth.pl
+++ examples/openid-auth.pl
@@ -11,33 +11,28 @@ use Plack;
 
 use Data::Dumper;
 
-sub session_path() { '/session' }
-
 my $app = sub {
     my $env = shift;
+    my $doorman = $env->{'doorman.openid'};
 
-    my $status = "Not Logged In";
+    my $status = $doorman->is_sign_in ? "Logged In As @{[ $doorman->verified_identity->display ]}" : "Not Logged In";
 
-    if ($_ = $env->{'doorman.openid.verified_identity'}) {
-        $status = "Logged In As @{[ $_->display ]}";
-    }
 
     return [200, ['Content-Type' => 'text/html'], [
-        qq{<html><body>},
-        qq{<nav><a href="/">Home</a></nav>},
+        qq{<html><body><nav>},
+        qq{<a href="/">Home</a> },
+        $doorman->is_sign_in ? qq{ <a href="@{[ $doorman->sign_out_path ]}">Logout</a>} : qq{},
+        qq{</nav>},
         qq{<p>$status</p>},
-        qq{<form method="post" action="@{[ session_path ]}">OpenID:<input type="text" name="openid" autofocus><input type="submit" value="Sign In"></form></html>},
+        qq{<form method="post" action="@{[ $doorman->sign_in_path ]}">OpenID:<input type="text" name="openid" autofocus><input type="submit" value="Sign In"></form></html>},
         '<hr><pre>' . Data::Dumper->Dump([$env], ['env']) . "</pre>",
         "</body></html>"
     ]];
 };
 
-# use Doorman::PlackMiddleware;
-# $app = Doorman::PlackMiddleware->wrap($app, root_url => 'http://localhost:5000');
-# $app;
-
 use Plack::Builder;
 builder {
-    enable "DoormanOpenID", root_url => 'http://localhost:5000';
+    enable "Session::Cookie";
+    enable "DoormanOpenID", root_url => 'http://localhost:5000', scope => 'users';
     $app;
 };
--- lib/Plack/Middleware/DoormanOpenID.pm
+++ lib/Plack/Middleware/DoormanOpenID.pm
@@ -7,17 +7,60 @@ use 5.010;
 use feature qw(say switch);
 
 use Plack::Request;
-use Plack::Util::Accessor qw(root_url);
+use Plack::Util::Accessor qw(root_url scope);
 
 use Net::OpenID::Consumer;
 use LWP::UserAgent;
 use Data::Dumper qw(Dumper);
 use Data::Thunk qw(lazy);
+use URI;
+use Scalar::Util qw(weaken);
 
-sub session_path { '/session' }
-sub session_url  {
+sub scope_uri  {
     my ($self) = @_;
-    $self->root_url . session_path()
+    $self->root_url . '/' . $self->scope()
+}
+
+sub sign_in_uri {
+    my ($self) = @_;
+    return $self->scope_uri . "/sign_in";
+}
+
+sub sign_out_uri {
+    my ($self) = @_;
+    return $self->scope_uri . "/sign_out";
+}
+
+sub openid_verified_uri {
+    my ($self) = @_;
+    return $self->scope_uri . "/openid_verified";
+}
+
+sub sign_in_path {
+    my ($self) = @_;
+    return URI->new($self->sign_in_uri)->path;
+}
+
+sub sign_out_path {
+    my ($self) = @_;
+    return URI->new($self->sign_out_uri)->path;
+}
+
+sub openid_verified_path {
+    my ($self) = @_;
+    return URI->new($self->openid_verified_uri)->path;
+}
+
+sub verified_identity {
+    my ($self) = @_;
+    my $env = $self->{env};
+    my $scope = $self->scope;
+    return $env->{"doorman.${scope}.openid.verified_identity"};
+}
+
+sub is_sign_in {
+    my ($self) = @_;
+    return defined $self->verified_identity;
 }
 
 sub csr {
@@ -33,10 +76,16 @@ sub csr {
 sub call {
     my ($self, $env) = @_;
 
+    $env->{'doorman.openid'} = $self;
+
+    $self->{env} = $env;
+    weaken($self->{env});
+
     my $request = Plack::Request->new($env);
+    my $session = $env->{'psgix.session'};
 
     given([$request->method, $request->path]) {
-        when(['POST', session_path]) {
+        when(['POST', $self->sign_in_path]) {
             my $csr = $self->csr($request);
             my $claimed_identity = $csr->claimed_identity( $request->param("openid") );
 
@@ -46,35 +95,47 @@ sub call {
 
             my $check_url = $claimed_identity->check_url(
                 delayed_return => 1,
-                return_to      => $self->session_url,
+                return_to      => $self->openid_verified_uri,
                 trust_root     => $self->root_url
             );
 
             return [302, ["Location" => $check_url], [""]];
         }
 
-        when(['GET', session_path]) {
+        when(['GET', $self->openid_verified_path]) {
             my $csr = $self->csr($request);
             $csr->handle_server_response(
                 verified => sub {
-                    $env->{'doorman.openid.verified_identity'} = shift;
-                    $env->{'doorman.openid.status'} = 'verified';
+                    my $id = shift;
+
+                    $env->{'doorman.'. $self->scope .'.openid.verified_identity'} = $id;
+                    $env->{'doorman.'. $self->scope .'.openid.status'} = 'verified';
+
+                    if ($session) {
+                        $session->{'doorman.'. $self->scope .'.openid.verified_identity_url'} = $id->url;
+                    }
                 },
                 setup_required => sub {
-                    $env->{'doorman.openid.status'} = 'setup_required';
+                    $env->{'doorman.'. $self->scope .'.openid.status'} = 'setup_required';
                 },
                 cancelled      => sub {
-                    $env->{'doorman.openid.status'} = 'cancelled';
+                    $env->{'doorman.'. $self->scope .'.openid.status'} = 'cancelled';
                 },
                 not_openid     => sub {
-                    $env->{'doorman.openid.status'} = 'not_openid';
+                    $env->{'doorman.'. $self->scope .'.openid.status'} = 'not_openid';
                 },
                 error          => sub {
                     my $err = shift;
-                    $env->{'doorman.openid.status'} = 'error';
-                    $env->{'doorman.openid.error'} = $err;
+                    $env->{'doorman.'. $self->scope .'.openid.status'} = 'error';
+                    $env->{'doorman.'. $self->scope .'.openid.error'} = $err;
                 }
             );
+        }
+
+        when(['GET', $self->sign_out_path]) {
+            if ($session) {
+                delete $session->{'doorman.'. $self->scope .'.openid.verified_identity_url'};
+            }
         }
     }
 
